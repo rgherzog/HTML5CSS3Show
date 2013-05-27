6fcf4500a5e1939e3a6403fc6783f431ffab1327
rgherzog <reinhart_herzog@yahoo.de>
2013-05-27 12:17:48 +0200

    WebWorker background thread

--- HTML5CSS3Show/Scripts/WebWorker.js
+++ HTML5CSS3Show/Scripts/WebWorker.js
@@ -1,14 +1,17 @@
-﻿//WebWorker
-$(function () {
-    var fibContainer = $("#fibContainerWorker");
+﻿$(function () {
+    //WebWorker
+    var fibContainerWorker = $("#fibContainerWorker");
     var worker;
+    var loadImg = $("#loadImgWorker");
+    loadImg.hide();
 
     $("#buttonStartWorker").click(function () {
         var fibCount = $("#fibCountWorker");
         var count = fibCount.val();
-        fibContainer.html("");
+        fibContainerWorker.html("");
+        loadImg.show();
 
-        var worker = new Worker("WebWorkerBackground.js");
+        var worker = new Worker("Scripts/WebWorkerBackground.js");
         worker.onmessage = messageHandler;
         worker.onerror = errorHandler;
         worker.postMessage(count);
@@ -17,12 +20,13 @@ $(function () {
     function messageHandler(e) {
         var results = e.data;
         $.each(results, function () {
-            logMessage(fibContainer, this);
+            logMessage(fibContainerWorker, this);
         });
+        loadImg.hide();
     }
-
+    
     function errorHandler(e) {
-        logMessage(fibContainer, e.message)
+        logMessage(fibContainerWorker, e.message)
     }
 });
 
@@ -39,29 +43,30 @@ $(function () {
         $.each(result, function () {
             logMessage(fibContainer, this);
         });
-    });       
+    });
+
+    //this function is faster than the recursive version of the background thread
+    //infact it takes longer to display the results than to calculate them. So to all appearances the background worker seem not to be working (infact the UI thread blocks because the background thread is so fast).
+    function generateFibonacci(count) {
+        var pprev, prev, temp;
+        var result = [];
+
+        pprev = 0;
+        prev = 1;
+        result.push(0);
+        result.push(1);
+
+        for (var i = 2; i < count; i++) {
+            temp = prev;
+            prev = prev + pprev;
+            pprev = temp;
+            result.push(prev);
+        }
+
+        return result;
+    }
 });
 
 function logMessage(container, value) {
     container.append("<li>" + value + "</li>");
 }
-
-function generateFibonacci(count) {
-    var pprev, prev, temp;
-    var result = [];
-
-    pprev = 0;
-    prev = 1;
-    result.push(0);
-    result.push(1);
-
-    for (var i = 2; i < count; i++) {
-        temp = prev;
-        prev = prev + pprev;
-        pprev = temp;
-        result.push(prev);
-    }
-
-    return result;
-}
-
--- HTML5CSS3Show/Scripts/WebWorkerBackground.js
+++ HTML5CSS3Show/Scripts/WebWorkerBackground.js
@@ -1,12 +1,32 @@
-﻿function messageHandler(e) {
+﻿var result = [];
+//very slow recursive function
+function calculateNextFibonacciValue(n) {
+    var s = 0;
+    var returnValue;
+
+    if (n == 0) {
+        return s;
+    }
+    if (n == 1) {
+        s += 1;
+        return (s);
+    }
+    else {
+        return calculateNextFibonacciValue(n - 1) + calculateNextFibonacciValue(n - 2);
+    }
+}
+
+function messageHandler(e) {
     if (e.data > 0) {
         generateFibonacciSeries(e.data);
     }
 }
 
 function generateFibonacciSeries(count) {
-    var results = [];
-    var result = generateFibonacci(e.data);
+    for (var i = 0; i < count; i++) {
+        result.push(calculateNextFibonacciValue(i));
+    }
+
     postMessage(result);
 }
 
